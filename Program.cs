37281ed2373aa1211fe34a83c8bf9b91b216e27b
Amina <aminchik2809@gmail.com>
2024-04-30 22:49:31 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿namespace ConsoleApp2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string myName = "Jane";
+            byte age = 27;
+            bool haveAPet = true;
+            double sizeOfFoot=37.5;
+            Console.WriteLine("My name is "+myName);
+            Console.WriteLine("My age is "+age);
+            Console.WriteLine("Do I have a pet "+haveAPet);
+            Console.WriteLine("My shoe size is "+ sizeOfFoot);
+        }
+    }
+}
